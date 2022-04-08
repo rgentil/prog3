ef33164f78751fd1d5bd6f83b40f0487ab678984
Ricardo Gentil <rgentil2112@gmail.com>
2022-04-08 14:24:45 -0300

    Se agrego ejercio 1 del practico 2

--- Practico_2/Recursivo_Filmina/src/Main.java
+++ Practico_2/Recursivo_Filmina/src/Main.java
@@ -14,10 +14,30 @@ public class Main {
 		int n = 3;
 		System.out.println("Factorial de " + n + " = " + recursividad.factorial(n));
 		
-		n = 5;
+		n = 50;
 		int y = 2;
 		System.out.println("Potencia de " + n + " al " + y + " = " + recursividad.potencia(n,y));
 		
+		int[] arr = {1,2,3,5,8,9,11,12,16,18,19,20,24,26,27,29,32,35,40,41,45,48,49,50};
+		
+		System.out.println("Arreglo size " + arr.length);
+		
+		System.out.println("El valor " + n + " se encuentra en la posición " + recursividad.busquedaBinaria(arr, n, 0, arr.length));
+	
+		
+		/*
+		Ejercicio 1.
+		Implemente un algoritmo recursivo que determine si un arreglo de tamaño N está ordenado.
+			1. ¿Qué complejidad O tiene? (La complejidad en el peor caso) O(n)
+			2. ¿Trae algún problema hacerlo recursivo? ¿Cuál? No
+			3. ¿Qué cambiaría si la estructura fuera una lista en lugar de un arreglo? Nada.
+
+		*/
+		int[] arrOrdenado = {1,2,5,8,9};
+		int[] arrDesordenado= {1,2,9,5,8,9};
+		System.out.println("El arreglo esta ordenado? " + recursividad.arregloOrdenado(arrOrdenado,0));
+		
+		System.out.println("El arreglo esta ordenado? " + recursividad.arregloOrdenado(arrDesordenado,0));
 	}
 
 }
--- Practico_2/Recursivo_Filmina/src/Recursividad.java
+++ Practico_2/Recursivo_Filmina/src/Recursividad.java
@@ -16,7 +16,7 @@ public class Recursividad {
 	}
 	
 	public int factorial(int n) {
-		if (n>1) { //Condicion de corte
+		if (n>1) { //Condición de corte
 			return n * factorial(n-1);
 		}else {
 			return 1;//Deja de calcular y empieza a desapilar
@@ -28,6 +28,34 @@ public class Recursividad {
 			return 1;
 		}else {
 			return n * potencia(n, y-1);
+		}
+	}
+	
+	public int busquedaBinaria(int[] arr, int x, int inicio, int fin) {
+		if (inicio > fin) {//Condición de corte
+			return -1;
+		}else {
+			int medio = (inicio + fin)/2;
+			if (x < arr[medio]) {
+				return busquedaBinaria(arr, x, inicio, medio - 1);//Recursivo con la primer mitad del arrelgo
+			}else {
+				if (x > arr[medio]) {
+					return busquedaBinaria(arr, x, medio + 1, fin);//Recursivo con la segunda mitad del arreglo
+				}else {
+					return medio;//Me retorna la posición del valor en el arreglo
+				}
+			}			
+		}
+	}
+	
+	public boolean arregloOrdenado(int[] arr, int x) {
+		if (arr.length <= 1 || arr.length <= x+1) {//Condición de corte
+			return true;
+		}
+		if (arr[x] > arr[x+1]) {//Condición de corte
+			return false;
+		}else {
+			return arregloOrdenado(arr, x+1);			
 		}
 	}
 	
