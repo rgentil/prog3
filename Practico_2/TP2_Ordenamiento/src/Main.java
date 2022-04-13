94d0bd8017526d943059fad2151ddf8bdca507b3
Ricardo Gentil <rgentil2112@gmail.com>
2022-04-13 14:00:37 -0300

    Se agrega orden por burbujeo

--- Practico_2/TP2_Ordenamiento/src/Main.java
+++ Practico_2/TP2_Ordenamiento/src/Main.java
@@ -4,12 +4,21 @@ public class Main {
 
 		Ordenamiento orden = new Ordenamiento();
 		
+		//Orden por seleccion menor
+		System.out.println("Orden por seleccion menor");
 		int[] arreglo = {2,6,82,1};
-		
 		int[] aOrdenado = orden.seleccion(arreglo);
-		
 		for (int i = 0; i < aOrdenado.length; i++) {
 			System.out.print(aOrdenado[i] + " ");
+		} 		
+		System.out.println("------------------------------------------------------------------");
+		
+		//Orden por burbugeo		
+		System.out.println("Orden por burbujeo ");
+		int[] arreglo1 = {4,7,8,4,3,2,6,82,1};
+		int[] aOrdenado1 = orden.burbujeo(arreglo1);
+		for (int i = 0; i < aOrdenado1.length; i++) {
+			System.out.print(aOrdenado1[i] + " ");
 		} 		
 	}
 
--- Practico_2/TP2_Ordenamiento/src/Ordenamiento.java
+++ Practico_2/TP2_Ordenamiento/src/Ordenamiento.java
@@ -3,6 +3,14 @@ public class Ordenamiento {
 
 	public Ordenamiento() {}
 	
+	/*
+	El método de ordenación por selección consiste en repetir los siguientes pasos:
+		Se busca el elemento más pequeño del array y se coloca en la primera posición.
+		Entre los restantes, se busca el elemento más pequeño y se coloca en la segunda posición.
+		Entre los restantes se busca el elemento más pequeño y se coloca en la tercera posición.
+	*/
+	
+	//Tiempo de ejecución O(n2)
 	public int[] seleccion(int a[]) {
 		for(int i=0; i < a.length-1; i++) { //Voy a recorrer todo el arreglo
 			int menor = a[i]; //Tomo como menor el primero y guardo el valor y la posicion en el que esta
@@ -18,12 +26,30 @@ public class Ordenamiento {
 					a[pos_menor] = aux;
 				}
 			}
-		}	
-		
-		return a;
-		 	
+		}			
+		return a;		 	
 	}
 	
+
+	/*
+	Consiste en comparar pares de elementos adyacentes en un array y si
+	están desordenados intercambiarlos hasta que estén todos ordenados.
+	El elemento mayor sube como una burbuja hacia la posición más alta. 
+	*/
+	//Tiempo de ejecución O(n2)
+	public int[] burbujeo(int a[]) {
+		for (int i=0; i<a.length-1;i++) {
+			for (int y=i+1; y<a.length;y++) {
+				if (a[i] > a[y]) {
+					int aux = a[i];
+					a[i] = a[y];
+					a[y] = aux;
+				}
+			}			
+		}
+		return a;
+		
+	}
 	
 	
 }
